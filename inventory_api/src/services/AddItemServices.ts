72def526d703154ad68df1d9707a5e80ca247051
esterandr02 <esterandr02@gmail.com>
2020-09-27 23:30:32 -0300

    basic add item service

---
+++ inventory_api/src/services/AddItemServices.ts
@@ -0,0 +1,18 @@
+import { inject, injectable } from 'tsyringe';
+
+import Request from '../dto/Request';
+import ItemRepositoryDTO from '../dto/repositories/ItemRepositoryDTO';
+
+import Item from '../entities/Item';
+
+@injectable()
+export default class AddItemService {
+    constructor(
+        @inject('ItemRepository')
+        private itemRepository: ItemRepositoryDTO
+    ) {}
+
+    public async execute({ item, quantity, value }: Request): Promise<Item> {
+        return { item, quantity, value };
+    }
+}
