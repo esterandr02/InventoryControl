bea26c748638fa28a2a2c16196eb1e43340dae59
esterandr02 <esterandr02@gmail.com>
2020-09-28 11:36:53 -0300

    typeorm Entity created

--- inventory_api/src/entities/Item.ts
+++ inventory_api/src/entities/Item.ts
@@ -1,1 +1,28 @@
-//export default class Entity
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('items')
+export default class Item {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    item: string;
+
+    @Column()
+    quantity: number;
+
+    @Column()
+    value: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
