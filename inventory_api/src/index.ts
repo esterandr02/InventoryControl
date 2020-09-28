04fa1752c16441ae6c6489464b30216b0fadd02b
esterandr02 <esterandr02@gmail.com>
2020-09-27 22:24:20 -0300

    import and use routes and alter api port

--- inventory_api/src/index.ts
+++ inventory_api/src/index.ts
@@ -1,10 +1,13 @@
 import 'express-async-errors';
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.listen(3000, () => {
+app.listen(3333, () => {
     console.log('🌐️ Server started on port 3000!');
 });
