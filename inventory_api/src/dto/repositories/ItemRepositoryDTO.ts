3e0e1bec848b09a405f273bceba9bae4fddb594c
esterandr02 <esterandr02@gmail.com>
2020-09-29 20:03:35 -0300

    update method excluded

--- inventory_api/src/dto/repositories/ItemRepositoryDTO.ts
+++ inventory_api/src/dto/repositories/ItemRepositoryDTO.ts
@@ -4,9 +4,7 @@ import Item from '../../entities/Item';
 export default interface ItemRepositoryDTO {
     create({ item, quantity, value }: Request): Promise<Item>;
 
-    update({ item, quantity, value }: Request): Promise<Item>;
-
-    remove(): Promise<void>;
+    remove(id: string): Promise<void>;
 
     save(item: Item): Promise<Item>;
 
