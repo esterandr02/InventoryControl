68ec88217256bc3daef39db79a03f1312f638882
esterandr02 <esterandr02@gmail.com>
2020-09-29 20:52:24 -0300

    validation routes and requests done

--- inventory_api/src/routes.ts
+++ inventory_api/src/routes.ts
@@ -1,14 +1,50 @@
 import { Router } from 'express';
 
+import { celebrate, Segments, Joi } from 'celebrate';
+
 const routes = Router();
 
 import ItemController from './controllers/ItemController';
 
 const itemController = new ItemController();
 
-routes.post('/inventory', itemController.create);
+routes.post(
+    '/inventory',
+    celebrate({
+        [Segments.BODY]: {
+            item: Joi.string(),
+            quantity: Joi.number().min(0).integer(),
+            value: Joi.number().min(0),
+        },
+    }),
+    itemController.create
+);
+
 routes.get('/inventory', itemController.list);
-routes.put('/inventory/:id', itemController.update);
-routes.delete('/inventory/:id', itemController.remove);
+
+routes.put(
+    '/inventory/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+        [Segments.BODY]: {
+            item: Joi.string(),
+            quantity: Joi.number().min(0).integer(),
+            value: Joi.number().min(0),
+        },
+    }),
+    itemController.update
+);
+
+routes.delete(
+    '/inventory/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    itemController.remove
+);
 
 export default routes;
