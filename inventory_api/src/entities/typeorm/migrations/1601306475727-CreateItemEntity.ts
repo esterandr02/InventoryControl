eb628298caabcf98d961d2d017d4ee52e9ee1039
esterandr02 <esterandr02@gmail.com>
2020-09-29 10:21:14 -0300

    item entity migration created

---
+++ inventory_api/src/entities/typeorm/migrations/1601306475727-CreateItemEntity.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateItemEntity1601306475727
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'items',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'item',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'quantity',
+                        type: 'int',
+                    },
+                    {
+                        name: 'value',
+                        type: 'int',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('items');
+    }
+}
