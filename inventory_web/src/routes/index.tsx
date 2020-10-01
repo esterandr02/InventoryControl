5dc84b0f7210e18802e732e6604b7ea201d7eafa
esterandr02 <esterandr02@gmail.com>
2020-10-01 09:06:44 -0300

    source connection between routes components

---
+++ inventory_web/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Inventory from '../pages/Inventory';
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Route path="/" exact component={Dashboard}></Route>
+        <Route path="/inventory" component={Inventory}></Route>
+    </Switch>
+);
+
+export default Routes;
